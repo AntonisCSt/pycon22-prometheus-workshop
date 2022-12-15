da8d079da699341d86b0d5ab504184860f427201
AntonisCSt <a.x-stellas@hotmail.com>
2022-12-15 18:09:55 +0200

    added metrics count

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ import requests
 from string import Template
 import time
 import random
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import HTTPServer
+from prometheus_client import MetricsHandler,Counter
 from util import artificial_503, artificial_latency
 
+requestCounter = Counter('requests_total', 'total number of requests', ['status', 'endpoint'])
 
 HOST_NAME = "0.0.0.0"  # This will map to available port in docker
 PORT_NUMBER = 8001
@@ -18,17 +20,19 @@ html_template = Template(html_string)
 
 def fetch_tree_count():
     r = requests.get(trees_api_url) if random.random() > 0.15 else artificial_503()
+    requestCounter.labels(status=r.status_code, endpoint='/upstream').inc()
     if r.status_code == 200:
         return r.json()["count"]
     return 0
 
 
-class HTTPRequestHandler(BaseHTTPRequestHandler):
+class HTTPRequestHandler(MetricsHandler):
     @artificial_latency
     def get_treecounter(self):
         self.do_HEAD()
         tree_count = fetch_tree_count()
         bytes_template = bytes(html_template.substitute(counter=tree_count), "utf-8")
+        requestCounter.labels(status="200", endpoint='/treecounter').inc()
         self.wfile.write(bytes_template)
 
     def do_HEAD(self):
@@ -40,6 +44,8 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         endpoint = self.path
         if endpoint == "/treecounter":
             return self.get_treecounter()
+        elif endpoint == '/metrics':
+            return super(HTTPRequestHandler, self).do_GET()
         else:
             self.send_error(404)
 
